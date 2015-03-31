be37de1fd6531271d04df42338bcdf60096231fd
Yaroslav <flayingfog@gmail.com>
2015-03-31 19:17:11 +0300

    upd admin page: change width for text,description fields, add inline to Question model

--- apps/polls/admin.py
+++ apps/polls/admin.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
+from django import forms
+
 from .models import Question, Answer, Poll, PollResult
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ('description', 'multipe_choice')
+        widgets = {
+            'description': forms.Textarea(attrs={'cols': 100, 'rows': 4})
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ('text', 'value', 'question_answers')
+        widgets = {
+            'text': forms.Textarea(attrs={'cols': 100, 'rows': 4})
+        }
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 0
+    form = AnswerForm
 
 class PollResultAdmin(admin.ModelAdmin):
     readonly_fields = ('date',)
 
-admin.site.register(Question)
-admin.site.register(Answer)
+class QuestionAdmin(admin.ModelAdmin):
+    form = QuestionForm
+    inlines = [ AnswerInline, ]
+
+class AnswerAdmin(admin.ModelAdmin):
+    form = AnswerForm
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
 admin.site.register(Poll)
 admin.site.register(PollResult, PollResultAdmin)
